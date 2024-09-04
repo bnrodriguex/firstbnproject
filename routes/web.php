8cc6cc415bbf7a310ea48eeb012a99f9a2a73cae
Brandon Rodrigues <bamerico968@gmail.com>
2024-09-03 21:49:54 -0300

    seed ver dnv

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 
-
-// Funcionário
+/* Funcionário */
 Route::get('/funcionario', [FuncionarioController::class, 'index']);
 
 
